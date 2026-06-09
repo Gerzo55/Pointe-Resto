30aeda76c8eaf58de3aa2b376c4cfedc01ed325a
Gerzo55 <gerzomakaya55@gmail.com>
2026-06-09 11:40:28 +0100

    Pointe-Resto Commit

---
+++ Backend/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO quartier (nom_quartier, description) VALUES
+('Centre-Ville', 'Le coeur de Pointe-Noire'),
+('Lumumba', 'Arrondissement 1 de Pointe-Noire'),
+('Tié-Tié', 'Arrondissement 3 de Pointe-Noire'),
+('Mpaka', 'Quartier situé dans le 6e arrondissement'),
+('Côte Sauvage', 'Zone côtière et touristique');
