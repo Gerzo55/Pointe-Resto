82d389f45f70600ad5f4ce96d669860e48b36526
Gerzo55 <gerzomakaya55@gmail.com>
2026-06-09 12:10:17 +0100

    Pointe-Resto Commit

--- Backend/seed.sql
+++ Backend/seed.sql
@@ -4,3 +4,13 @@ INSERT INTO quartier (nom_quartier, description) VALUES
 ('Tié-Tié', 'Arrondissement 3 de Pointe-Noire'),
 ('Mpaka', 'Quartier situé dans le 6e arrondissement'),
 ('Côte Sauvage', 'Zone côtière et touristique');
+
+INSERT INTO utilisateur (nom, email, mot_de_passe, role) VALUES
+('Gerzo MAKAYA', 'gerzomakaya55@gmail.com', '126457645lk', 'moderateur'),
+('Précieux KIMINOU', 'precieuxrelkiminou@gmail.com', 'Admin3506l', 'restaurateur'),
+('Gotron MBOMO','@gmail.com','Gotron564','Restaurateur'),
+('Selma MOULOUANDA','tiphaniemoun@gmail.com','SM756SD','Restaurateur'),
+('Djowen MAKOSSO', 'bigbossking@gmail.com', 'Djowen97J7Y', 'client'),
+('Monica OPPELET','monicaoppelet@gmail.com','Monica75TF','Client'),
+('Izya LOCKO','lockoizya@gmail.com','izya132LK','Client'),
+('','@gmail.com','','Client'),
